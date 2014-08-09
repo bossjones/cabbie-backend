8898867ce009244ee99cdb97d347e329af6062e9
Minjong Chung <mjipeo@gmail.com>
2014-08-10 03:10:22 +0900

    Add fabric deplayment and supervisor conf

---
+++ fabfile.py
@@ -0,0 +1,43 @@
+import os
+
+from fabric.api import env, cd, execute, roles, run, sudo
+
+
+LOCAL_DIR = os.path.realpath(os.path.dirname(__file__))
+PROD_DIR = '/home/cabbie/workspace/cabbie-backend'
+ACTIVATE = '/home/cabbie/.virtualenvs/cabbie/bin/activate'
+
+env.roledefs = {
+    'prod': ['lee1469.cafe24.com'],
+}
+env.user = 'cabbie'
+env.password = 'roqkfwk1'
+
+def prepare():
+    with cd(LOCAL_DIR):
+        pass
+
+@roles('prod')
+def deploy_web():
+    with cd(PROD_DIR):
+        run('git pull origin master')
+        sudo('supervisorctl restart cabbie-web')
+
+@roles('prod')
+def deploy_celery():
+    with cd(PROD_DIR):
+        run('git pull origin master')
+        sudo('supervisorctl restart cabbie-celery')
+        sudo('supervisorctl restart cabbie-celerybeat')
+
+@roles('prod')
+def deploy_location():
+    with cd(PROD_DIR):
+        run('git pull origin master')
+        sudo('supervisorctl restart cabbie-location')
+
+def deploy():
+    prepare()
+    execute(deploy_web)
+    execute(deploy_celery)
+    execute(deploy_location)
