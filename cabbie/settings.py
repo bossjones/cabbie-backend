d88366656206b53f31761cd02086dc8b332816ab
Minjong Chung <mjipeo@gmail.com>
2014-09-27 23:48:54 +0900

    Add OrderingFilter to settings.py

--- cabbie/settings.py
+++ cabbie/settings.py
@@ -237,10 +237,15 @@ CELERYBEAT_SCHEDULE = {}
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',),
+        'rest_framework.authentication.TokenAuthentication',
+    ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',),
-    'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_FILTER_BACKENDS': (
+        'rest_framework.filters.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
+    ),
     'PAGINATE_BY': 20,
 }
 
