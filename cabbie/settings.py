edb40f320d27ab7992fbf0f1724c35a80c168c6f
Minjong Chung <mjipeo@gmail.com>
2014-10-18 23:53:09 +0900

    Configure cache

--- cabbie/settings.py
+++ cabbie/settings.py
@@ -104,10 +104,13 @@ DATABASES = {
     },
 }
 
-# FIXME: Should be configured for production cache
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'BACKEND': 'redis_cache.cache.RedisCache',
+        'LOCATION': '127.0.0.1:6379:1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'redis_cache.client.DefaultClient',
+        },
     }
 }
 
