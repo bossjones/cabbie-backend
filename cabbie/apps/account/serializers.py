2c61f40bb00273bc60d076e858ff0c34820cf132
Juno Kim <kokookko1@gmail.com>
2014-10-18 20:09:11 +0900

    Revert "Send rating push message when ride completed"
    This reverts commit f730297431ca83022cbac5861a424324680d052f.

--- cabbie/apps/account/serializers.py
+++ cabbie/apps/account/serializers.py
@@ -29,19 +29,17 @@ class UserSerializer(AbstractSerializer):
     class Meta:
         model = User
         fields = ('id', 'phone', 'password', 'name', 'point', 'date_joined',
-                  'remain_days_for_promotion', 'recommend_code')
-        read_only_fields = ('point', 'date_joined', 'recommend_code')
+                  'remain_days_for_promotion')
+        read_only_fields = ('point', 'date_joined')
         write_only_fields = ('password',)
 
 
 class PassengerSerializer(UserSerializer):
     class Meta(UserSerializer.Meta):
         model = Passenger
-        fields = UserSerializer.Meta.fields + ('email', 'ride_count', 'parse_installation_object_id')
+        fields = UserSerializer.Meta.fields + ('email', 'ride_count')
         read_only_fields = UserSerializer.Meta.read_only_fields \
                            + ('ride_count',)
-        write_only_fields = UserSerializer.Meta.write_only_fields \
-                            + ('parse_installation_object_id',)
 
 
 class DriverSerializer(UserSerializer):
--- cabbie/apps/drive/signals.py
+++ cabbie/apps/drive/signals.py
@@ -2,4 +2,3 @@ import django.dispatch
 
 
 post_ride_board = django.dispatch.Signal(providing_args=['ride'])
-post_ride_complete = django.dispatch.Signal(providing_args=['ride'])
