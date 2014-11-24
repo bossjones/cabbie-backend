2c8236b02f4fc8624c45128654d13e20473368a6
Juno Kim <kokookko1@gmail.com>
2014-11-24 15:40:32 +0900

    Change cache backend to use database

--- cabbie/settings.py
+++ cabbie/settings.py
@@ -111,12 +111,10 @@ DATABASES = {
 
 CACHES = {
     'default': {
-        'BACKEND': 'redis_cache.cache.RedisCache',
-        'LOCATION': '127.0.0.1:6379:1',
-        'OPTIONS': {
-            'CLIENT_CLASS': 'redis_cache.client.DefaultClient',
-        },
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'cache_table',
     }
+
 }
 
 LOGGING = {
