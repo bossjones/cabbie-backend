ec49a05be44095fad881ff4db13929b5541f8a32
Juno Kim <kokookko1@gmail.com>
2014-12-18 17:21:53 +0900

    Refine appversion admin

--- cabbie/apps/appversion/admin.py
+++ cabbie/apps/appversion/admin.py
@@ -4,5 +4,6 @@ from cabbie.apps.appversion.models import AndroidDriver
 
 class AndroidDriverAdmin(admin.ModelAdmin):
     list_display = ('version_code', 'version_name', 'is_update_required', 'description')
+    fields = ('version_code', 'version_name', 'is_update_required', 'description' )
 
 admin.site.register(AndroidDriver, AndroidDriverAdmin)
