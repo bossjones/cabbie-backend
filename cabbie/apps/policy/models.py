8a7e7f8e5fb6d8e91d8b74a57c092fbcd087891d
Juno Kim <kokookko1@gmail.com>
2014-12-18 18:01:11 +0900

    Split location information to policy app

---
+++ cabbie/apps/policy/models.py
@@ -0,0 +1,47 @@
+# encoding: utf8
+import operator
+
+from django.db import models
+
+from cabbie.apps.account.models import User
+from cabbie.common.models import AbstractTimestampModel
+
+class LocationDataAccess(AbstractTimestampModel):
+    user = models.ForeignKey(User, related_name='location_data_accesses',
+                             verbose_name=u'접근한 사용자')
+
+    class Meta(AbstractTimestampModel.Meta):
+        verbose_name = u'위치정보 접근로그'
+        verbose_name_plural = u'위치정보 접근로그'
+
+
+class LocationDataProvide(AbstractTimestampModel):
+    provider = models.ForeignKey(
+        User, related_name='location_data_provider_history',
+        verbose_name=u'제공한자')
+    providee = models.ForeignKey(
+        User, related_name='location_data_providee_history',
+        null=True, blank=True, verbose_name=u'제공받은자')
+    provider_type = models.CharField(max_length=20, verbose_name=u'취득경로')
+    service = models.CharField(max_length=20, verbose_name=u'제공서비스')
+
+    class Meta(AbstractTimestampModel.Meta):
+        verbose_name = u'위치정보 이용제공내역'
+        verbose_name_plural = u'위치정보 이용제공내역'
+
+
+class LocationDataNotice(AbstractTimestampModel):
+    noticer = models.ForeignKey(
+        User, related_name='location_data_noticer_history',
+        verbose_name=u'취급자')
+    requester = models.ForeignKey(
+        User, related_name='location_data_requester_history',
+        verbose_name=u'요청자')
+    purpose = models.CharField(max_length=100, verbose_name=u'목적')
+
+    class Meta(AbstractTimestampModel.Meta):
+        verbose_name = u'위치정보 열람및고지내역'
+        verbose_name_plural = u'위치정보 열람및고지내역'
+
+
+from cabbie.apps.stats.receivers import *
--- cabbie/apps/stats/admin.py
+++ cabbie/apps/stats/admin.py
@@ -4,9 +4,7 @@ from django.contrib import admin
 from django import forms
 
 from cabbie.apps.stats.models import (
-    DriverRideStatMonth, DriverRideStatWeek, DriverRideStatDay, 
-    LocationDataAccess, LocationDataProvide,
-    LocationDataNotice)
+    DriverRideStatMonth, DriverRideStatWeek, DriverRideStatDay)
 from cabbie.common.admin import AbstractAdmin
 
 
@@ -68,47 +66,6 @@ class DriverRideStatDayAdmin(AbstractAdmin):
         'driver__name',
     )
 
-
-class LocationDataAccessAdmin(AbstractAdmin):
-    list_display = ('user', 'created_at_', 'updated_at_')
-
-    def created_at_(self, obj):
-        return obj.created_at
-
-    def updated_at_(self, obj):
-        return obj.updated_at
-
-    created_at_.short_description = u'로그인'
-    updated_at_.short_description = u'로그아웃'
-
-
-class LocationDataProvideAdmin(AbstractAdmin):
-    list_display = ('provider', 'provider_type', 'service','providee',
-                    'created_at_')
-
-    def created_at_(self, obj):
-        return obj.created_at
-
-    created_at_.short_description = u'제공시간'
-
-
-class LocationDataNoticeAdmin(AbstractAdmin):
-    list_display = ('noticer', 'requester', 'purpose',
-                    'created_at_', 'updated_at_')
-
-    def created_at_(self, obj):
-        return obj.created_at
-
-    def updated_at_(self, obj):
-        return obj.updated_at
-
-    created_at_.short_description = u'요청시간'
-    updated_at_.short_description = u'고지시간'
-
-
 admin.site.register(DriverRideStatMonth, DriverRideStatMonthAdmin)
 admin.site.register(DriverRideStatWeek, DriverRideStatWeekAdmin)
 admin.site.register(DriverRideStatDay, DriverRideStatDayAdmin)
-admin.site.register(LocationDataAccess, LocationDataAccessAdmin)
-admin.site.register(LocationDataProvide, LocationDataProvideAdmin)
-admin.site.register(LocationDataNotice, LocationDataNoticeAdmin)
