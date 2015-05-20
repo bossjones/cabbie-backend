2574a1a6d3c8724aa384a7ccecc3cf5c9d94682d
Juno Kim <kokookko1@gmail.com>
2015-05-20 16:12:36 +0900

    Rename account migration file to avoid duplicate

--- cabbie/apps/account/migrations/0011_passenger_affiliation.py
+++ cabbie/apps/account/migrations/0011_passenger_affiliation.py
@@ -8,8 +8,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('account', '0008_auto_20150403_1514'),
-        ('affiliation', '__first__'),
+        ('account', '0010_auto_20150512_2205'),
     ]
 
     operations = [
@@ -18,10 +17,5 @@ class Migration(migrations.Migration):
             name='affiliation',
             field=models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, verbose_name='\uc81c\ud734\uc0ac', to='affiliation.Affiliation', null=True),
             preserve_default=True,
-        ),
-        migrations.AlterField(
-            model_name='driver',
-            name='region',
-            field=models.CharField(default=b'', max_length=20, verbose_name='\uc9c0\uc5ed', blank=True),
         ),
     ]
