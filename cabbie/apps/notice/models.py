64584dbaf5024a419af1da63d989ccb7ed6de1fc
Juno Kim <kokookko1@gmail.com>
2015-05-28 13:05:22 +0900

    Fix for notice content field

--- cabbie/apps/notice/models.py
+++ cabbie/apps/notice/models.py
@@ -8,7 +8,7 @@ from cabbie.common.models import (ActiveMixin, NullableImageMixin,
 
 class Notice(ActiveMixin, AbstractTimestampModel):
     title = models.CharField(u'제목', max_length=50)
-    content = tinymce_models.HTMLField(u'내용')
+    content = tinymce_models.HTMLField(u'내용', blank=True)
     visible_from = models.DateTimeField(u'게시시간') 
 
     class Meta(ActiveMixin.Meta, AbstractTimestampModel.Meta):
@@ -17,7 +17,7 @@ class Notice(ActiveMixin, AbstractTimestampModel):
        
 class AppPopup(ActiveMixin, NullableImageMixin, AbstractTimestampModel): 
     title = models.CharField(u'제목', max_length=50)
-    content = tinymce_models.HTMLField(u'내용')
+    content = tinymce_models.HTMLField(u'내용', blank=True)
     link = models.URLField(u'상세링크', max_length=200)
     starts_at= models.DateTimeField(u'시작시간', null=True, db_index=True)
     ends_at = models.DateTimeField(u'종료시간', null=True, db_index=True)
