bdbd24492455e829b20d3d4afcab6aadc1bc231b
Juno Kim <kokookko1@gmail.com>
2015-05-29 21:25:32 +0900

    Add ios app version admin

--- cabbie/apps/appversion/admin.py
+++ cabbie/apps/appversion/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from cabbie.apps.appversion.models import AndroidDriver, AndroidPassenger
+from cabbie.apps.appversion.models import AndroidDriver, AndroidPassenger, IosPassenger
 
 class AndroidDriverAdmin(admin.ModelAdmin):
     list_display = ('version_code', 'version_name', 'is_update_required', 'description')
@@ -10,6 +10,10 @@ class AndroidPassengerAdmin(admin.ModelAdmin):
     list_display = ('version_code', 'version_name', 'is_update_required', 'description')
     fields = ('version_code', 'version_name', 'is_update_required', 'description' )
 
+class IosPassengerAdmin(admin.ModelAdmin):
+    list_display = ('version_code', 'version_name', 'is_update_required', 'description')
+    fields = ('version_code', 'version_name', 'is_update_required', 'description' )
 
 admin.site.register(AndroidDriver, AndroidDriverAdmin)
 admin.site.register(AndroidPassenger, AndroidPassengerAdmin)
+admin.site.register(IosPassenger, IosPassengerAdmin)
