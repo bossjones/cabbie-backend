34abc51411a8ef08a0cd89c505422daa8e15df54
Juno Kim <kokookko1@gmail.com>
2015-08-19 14:09:56 +0900

    Change default form widget for ManyToManyField in admin

--- cabbie/common/admin.py
+++ cabbie/common/admin.py
@@ -1,5 +1,7 @@
 from daterange_filter.filter import DateRangeFilter as BaseDateRangeFilter
+from django.db import models
 from django.contrib import admin
+from django.forms import CheckboxSelectMultiple
 from import_export.admin import ExportMixin
 
 
@@ -9,6 +11,10 @@ class AbstractAdmin(ExportMixin, admin.ModelAdmin):
 
     actions_on_top = True
     actions_on_bottom = False 
+
+    formfield_overrides = {
+        models.ManyToManyField: {'widget': CheckboxSelectMultiple},
+    }
 
     def get_actions(self, request):
         actions = super(AbstractAdmin, self).get_actions(request)
