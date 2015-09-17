d66165e1b19d664e99ab335a53c85d47c2363725
Juno Kim <kokookko1@gmail.com>
2015-09-17 18:02:00 +0900

    Add new admin Notice, and add new fields "notice_type", "link"

--- cabbie/apps/notice/admin.py
+++ cabbie/apps/notice/admin.py
@@ -6,20 +6,17 @@ from cabbie.apps.notice.models import Notice, AppPopup
 from cabbie.common.admin import AbstractAdmin
 
 class NoticeAdmin(AbstractAdmin):
-    change_form_template = 'notice/admin/change_form.html'
-
-    list_display = ('id', 'title', 'visible_from', 'created_at')
+    list_display = ('id', 'title', 'notice_type', 'link', 'visible_from', 'created_at')
     search_fields = (
         'content',
     )
-    fields = ('id', 'title', 'content', 'visible_from', 'is_active',)
+    list_filter = ('notice_type',)
+    fields = ('id', 'title', 'notice_type', 'content', 'link', 'visible_from', 'is_active',)
     readonly_fields = (
         'id',
     ) 
 
 class AppPopupAdmin(AbstractAdmin):
-    change_form_template = 'notice/admin/change_form.html'
-
     list_display = ('id', 'title', 'image_preview', 'link', 'starts_at', 'ends_at', 'status')
     search_fields = (
         'title',
@@ -48,5 +45,5 @@ class AppPopupAdmin(AbstractAdmin):
     status.short_description = u'상태'
         
 
-
 admin.site.register(AppPopup, AppPopupAdmin)
+admin.site.register(Notice, NoticeAdmin)
--- cabbie/apps/notice/models.py
+++ cabbie/apps/notice/models.py
@@ -7,8 +7,17 @@ from cabbie.common.models import (ActiveMixin, NullableImageMixin,
                                   AbstractTimestampModel)
 
 class Notice(ActiveMixin, AbstractTimestampModel):
+    TYPE_EVENT, TYPE_UPDATE, TYPE_NEWS = 'event', 'update', 'news'
+    NOTICE_TYPES = (
+        (TYPE_EVENT,    u'이벤트'),
+        (TYPE_UPDATE,   u'업데이트'),
+        (TYPE_NEWS,     u'백기사 뉴스'),
+    )
+
     title = models.CharField(u'제목', max_length=50)
     content = tinymce_models.HTMLField(u'내용', blank=True)
+    notice_type = models.CharField(u'타입', max_length=10, choices=NOTICE_TYPES, default=TYPE_EVENT)
+    link = models.URLField(u'상세링크', max_length=200, blank=True, null=True)
     visible_from = models.DateTimeField(u'게시시간') 
 
     class Meta(ActiveMixin.Meta, AbstractTimestampModel.Meta):
